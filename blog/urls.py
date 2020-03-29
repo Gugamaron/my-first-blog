676aced662bbb29bc41a298be20dc044a188051c
gustavo <gustavosilvamaciel.gsm1705@gmail.com>
2020-03-29 12:49:16 -0300

    modificação nas urls.

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,4 @@ urlpatterns = [
 	path('', views.post_list, name='post_list'),
 	path('post/<int:pk>/', views.post_detail, name='post_detail'),
 	path('post/new/', views.post_new, name='post_new'),
-	#path('', include('blog.urls')),
-	path('admin/', admin.site.urls),
-	path('account/', include('django.contrib.auth.urls')),
 ]
